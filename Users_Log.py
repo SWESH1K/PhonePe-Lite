36f26aedfe6655b0de1536255de4cc33f9acbaf1
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-03-11 11:16:47 +0530

    Ok

--- Users_Log.py
+++ Users_Log.py
@@ -2,16 +2,16 @@ users = {
     "Sweshik": {
         "password": "password1",
         "pin": 1234,
-        "balance": 51114
+        "balance": 61114
     },
     "Lucky": {
         "password": "password2",
         "pin": 4321,
-        "balance": 75224
+        "balance": 65224
     },
     "Zaid": {
         "password": "password3",
         "pin": 5678,
-        "balance": 47645
+        "balance": 47425
     }
 }
