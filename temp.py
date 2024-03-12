442709b77c2a3b236e3444e48b3dcc5c2e36fd5d
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-03-12 12:07:36 +0530

    Ok

--- temp.py
+++ temp.py
@@ -1,6 +1,36 @@
-import csv
+from time import *
 
-data = csv.DictReader(open('sample.csv'))
-my_list = list(data)
+def internet(function):
+    def wrapper(*args):
+        print("Hello",args[0])
+        function(*args)
 
-for i in my_list: print(i)
+    return wrapper
+
+def cache(function):
+    cacheData = set()
+    def wrapper(name):
+        if (name in cacheData):
+            print("Already There!")
+        else:
+            sleep(2)
+            cacheData.add(name)
+            print(name, "Added!")
+    return wrapper
+
+# @internet
+@cache
+def hello(name):
+    print("How are you?",name)
+
+
+def main():
+    hello("Lucky")
+    # hello("Lucky")
+    hello("sweshik")
+    # hello("sweshik")
+
+
+
+if __name__ == '__main__':
+    main()
